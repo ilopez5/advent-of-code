5b878ef9db3741891c4959bfe64854515320bfa7
ilopez5 <ilopez5@hawk.iit.edu>
2020-12-23 20:19:09 -0600

    day19: part 2 complete. apparently recursive depth is no more than 3, so guessing paid off :)

--- 2020/day19/monster-messages.py
+++ 2020/day19/monster-messages.py
@@ -5,12 +5,14 @@ import re
 
 memoization = dict()
 
+depth = dict()
+
 def parse(filename):
     with open(filename, "r") as fd:
         data = fd.read().splitlines()
     return data
 
-def ruleReplace(data, ruleNum):
+def ruleReplace(data, ruleNum, part=1):
     # parse line
     rules = re.findall(r'([\d]+|\||[a-z])', data[ruleNum])
     main  = int(rules[0])
@@ -19,16 +21,24 @@ def ruleReplace(data, ruleNum):
     if main in memoization:
         return memoization[main]
 
+
     rules = rules[1:]
     if re.match(r'[a-z]', rules[0]):
         # reached char endpoint (leaf)
         memoization[main] = rules[0]
         return rules[0]
     else:
+        # assume input data won't go past 3 levels of recursion
+        if part == 2 and ruleNum in [8,11]:
+            if depth[ruleNum] > 3:
+                return memoization[42]
+            else:
+                depth[ruleNum] += 1
+
         # more rules (non-leaf)
         for r, rule in enumerate(rules):
             if rules[r] != '|':
-                rules[r] = ruleReplace(data, int(rule))
+                rules[r] = ruleReplace(data, int(rule), part)
         # wrap this group
         if '|' in rules and main > 0:
             rules = ['('] + rules + [')']
@@ -52,7 +62,9 @@ def partTwo(data):
     data[8]  = "8: 42 | 42 8"
     data[11] = "11: 42 31 | 42 11 31"
     memoization.clear()
-    pattern = re.compile(ruleReplace(data, 0))
+    depth[8] = 0
+    depth[11] = 0
+    pattern = re.compile(ruleReplace(data, 0, 2))
     
     # run each monster message through regex
     count = 0
